539a1c1ff4b61894e871159d33b65b9eba7b105f
Glynn Bird <glynn.bird@gmail.com>
2023-05-30 09:08:22 +0100

    rebase

--- bin/couchsnap.bin.js
+++ bin/couchsnap.bin.js
@@ -1,17 +1,74 @@
 #!/usr/bin/env node
 
-// command-line args
+const syntax = 
+`Syntax:
+--url/-u           (COUCH_URL)      CouchDB URL          (required)
+--database/--db/-d (COUCH_DATABASE) CouchDB Datbase name (required)
+--deletions                         Include deleted docs (default: false)
+--version/v                         Show app version     (default: false)
+`
 const url = process.env.COUCH_URL || 'http://localhost:5984'
 const db = process.env.COUCH_DATABASE
-const args = require('yargs')
-  .option('url', { alias: 'u', describe: 'CouchDB URL', default: url })
-  .option('database', { alias: ['db', 'd'], describe: 'CouchDB database name', demandOption: !db, default: db })
-  .option('deletions', { describe: 'Include deletions', type: 'boolean', demandOption: false, default: false })
-  .help('help')
-  .argv
+const app = require('../package.json')
+const { parseArgs } = require('node:util')
+const argv = process.argv.slice(2)
+const options = {
+  url: {
+    type: 'string',
+    short: 'u',
+    default: url
+  },
+  database: {
+    type: 'string',
+    short: 'd',
+    default: db
+  },
+  db: {
+    type: 'string',
+    default: db
+  },
+  deletions: {
+    type: 'boolean',
+    default: false
+  },
+  version: {
+    type: 'boolean',
+    short: 'v',
+    default: false
+  },
+  help: {
+    type: 'boolean',
+    short: 'h',
+    default: false
+  }
+}
+
+// parse command-line options
+const { values } = parseArgs({ argv, options })
+if (values.db) {
+  values.database = values.db
+  delete values.db
+}
+// version mode
+if (values.version) {
+  console.log(`${app.name} ${app.version}`)
+  process.exit(0)
+}
+
+// help mode
+if (values.help) {
+  console.log(syntax)
+  process.exit(0)
+}
+
+// must supply URL & database
+if (!values.url || !values.database) {
+  console.error(syntax, 'Error: You must supply a url and database name')
+  process.exit(1)
+}
 
 // start the snapshot
 const couchsnap = require('../index.js')
-couchsnap.start(args)
+couchsnap.start(values)
   .then(console.log)
   .catch(console.error)
